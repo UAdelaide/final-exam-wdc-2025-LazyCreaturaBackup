159185cd01a77df17cbf96b51caafd3f66bdc085
LazyCreaturaBackup <afkpixelgun@gmail.com>
2025-06-21 06:54:31 +0000

    [automatic commit] 2025-06-21 06:54:30 UTC 20.227.140.176

--- part2/app.js
+++ part2/app.js
@@ -44,6 +44,10 @@ app.use(session({
         maxAge: 24 * 60 * 1000 // 24 minutes
     }
 }));
+// Simple logout route that redirects to login
+app.get('/logout', (req, res) => {
+  res.redirect('/');
+});
 
 // Export the app instead of listening here
 module.exports = app;
