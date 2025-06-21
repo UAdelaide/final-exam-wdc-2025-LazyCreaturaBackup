f1550fa819850bda5393ad4002229c8036598f6d
LazyCreaturaBackup <afkpixelgun@gmail.com>
2025-06-21 06:53:10 +0000

    [automatic commit] 2025-06-21 06:53:10 UTC 20.227.140.176

--- part2/app.js
+++ part2/app.js
@@ -41,7 +41,7 @@ app.use(session({
     cookie: {
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+        maxAge: 24 * 60 * 1000 // 24 minutes
     }
 }));
 
