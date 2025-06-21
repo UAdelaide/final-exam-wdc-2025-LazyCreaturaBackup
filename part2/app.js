35a04dfb678f94b4e97cc6e680a333ec899f1615
LazyCreaturaBackup <afkpixelgun@gmail.com>
2025-06-21 06:30:06 +0000

    [automatic commit] 2025-06-21 06:30:06 UTC 20.227.140.176

--- part2/app.js
+++ part2/app.js
@@ -3,7 +3,15 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-
+// Route to return users as JSON
+app.get('/api/user', async (req, res) => {
+  try {
+    const [books] = await db.execute('SELECT * FROM books');
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
+});
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
